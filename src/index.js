088e04d4053ca1621b2af1fadcd4b71ae0435cb2
Warren Wei <warrenwei1108@gmail.com>
2019-08-13 19:43:17 +1000

    Setup the task creation endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose')
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.port || 3000;
@@ -12,8 +13,18 @@ app.post('/users', (req, res) => {
     
     user.save().then(() => {
         res.send(user);
-    }).catch(() => {
-        
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.send(task);
+    }).catch((e) => {
+        res.send(e);
     });
 });
 
